db5cba7be21960fe3f7493acb22f6a6a0b7de7bc
lhellings <lbartran@gmail.com>
2022-01-02 14:53:49 -0600

    done

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8080;
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+   console.log('Server is up!');
+});
+//had to use 8080, did not work with 3000
